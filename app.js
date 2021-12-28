29c5ee4076342cb2e53c26e3fc9be2c0d20def6f
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-28 23:19:33 +0430

    sample data and req, res perfomed for v1 of api

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+const fs = require('fs')
 const express = require('express')
+
 const app = express()
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello from the server')
+})
+
+const tours =JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        stauts: 'success',
+        result: tours.lenght,
+        data: {
+            tours
+        }
+    })
 })
 
 const port = 8001
