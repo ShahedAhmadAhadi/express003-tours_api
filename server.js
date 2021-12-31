471334633ac2d9e21c1fe5f8384ae392d2573584
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-31 20:02:32 +0430

    new enviroment and new script added

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const dotenv = require('dotenv')
+dotenv.config({ path: './config.env' })
 const app = require("./app")
 
-dotenv.config()
 
 process.env.NODE_ENV="development"
-console.log(process.env)
+// console.log(process.env)
 
-const port = 8001
+const port = process.env.PORT || 8001
 app.listen(port, ()=> {
     console.log(`App running on port ${port}`)
 })
