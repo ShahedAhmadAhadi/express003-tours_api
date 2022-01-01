870793c3582b43e3d4e719176df961784ece92f7
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-01 23:48:05 +0430

    mongoose added and database connection with both local and atlas successfull and Schema for tour with it's model

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config({ path: './config.env' })
 const app = require("./app")
 
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+// mongoose.connect(process.env.DATABASE_LOCAL, {
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+}).then(() => {
+    console.log('db successfull')
+})
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+})
+
+const tour = mongoose.model('Tour', tourSchema)
 
 process.env.NODE_ENV="development"
 // console.log(process.env)
