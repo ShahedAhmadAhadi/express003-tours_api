f174e8104f426c19d7bfb3f248da678b269b94a9
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-02 18:48:21 +0430

    sorting field limiting and pagination added with a common route with valuble data as an alias in middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -8,6 +8,13 @@ const Tour = require('../models/tourModle')
 //     next()
 // }
 
+exports.aliasTopTours = async (req, res, next) => {
+    req.query.limit = '5'
+    req.query.sort = '-ratingsAverage,price'
+    req.query.fields = 'name, price, ratingsAverage, summary, difficulty'
+    next()
+}
+
 exports.getAllTours = async (req, res) => {
     try {
         // Build Query
@@ -19,7 +26,34 @@ exports.getAllTours = async (req, res) => {
         let queryStr = JSON.stringify(queryObj)
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
 
-        const query = Tour.find(JSON.parse(queryStr))
+        let query = Tour.find(JSON.parse(queryStr))
+
+        // 3) sorting
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ')
+            query = query.sort(sortBy)
+        }else {
+            query = query.sort('-createdAt')
+        }
+
+        // 4) field limiting
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields)
+        } else {
+            query = query.select('-__v')
+        }
+
+        // 5) pagination
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit)
+        if(req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if(skip >= numTours) throw new Error('This page not exist')
+        }
+
         // Execute Query
         const tours = await query
         // Response
