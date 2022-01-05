686a889dbcab30953e2e1d0bd97f940718dd046a
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-06 03:37:26 +0430

    reset password email and token defined for reset in user model created and deleted success

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,10 @@
 const {promisify} = require('util')
+const crypto = require('crypto')
 const jwt = require("jsonwebtoken")
 const User = require("../models/userModel")
 const AppError = require("../utils/appError")
 const catchAsync = require("../utils/catchAsync")
+const sendEmail = require("../utils/email")
 
 const signToken = id => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -85,9 +87,52 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
         return next(new AppError('There is no user with this email', 404)) 
     }
 
-    const resetToken = user.createResetPasswordToken()
+    const resetToken = await user.createPasswordResetToken()
     await user.save({validateBeforeSave: false})
+
+    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/user/resetPassword/${resetToken}`
+    const message = `Forgot your password? submit your PATCH request with new password and confirmPassword to: ${resetURL}.\n
+    If you didn't forget your password, please ignore this email`
+    await sendEmail({
+        email: user.email,
+        subject: 'Your password reset Token (10 minutes)',
+        message
+    })
+    try {
+    } catch (err) {
+        user.passwordResetToken = undefined;
+        user.passwordResetExpires = undefined;
+        await user.save({validateBeforeSave: false})
+
+        return next(new AppError('There was error sending email. Try again later'), 500)
+    }
+
+    res.status(200).json({
+        success: 'success',
+        message: 'Token sent to email!'
+    })
 
 })
 
-exports.resetPassword = (req, res, next)
+exports.resetPassword = async (req, res, next) => {
+    const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex')
+
+    const user = await User.findOne({passwordResetToken: hashedToken, passwordResetExpires: {$gt: Date.now()}})
+
+    if(!user) {
+        return next(new AppError('Token is Invalid or has Expired', 400))
+    }
+
+    user.password = req.body.password
+    user.passwordConfirm = req.body.passwordConfirm
+    user.passwordResetToken = undefined
+    user.passwordResetExpires = undefined
+    await user.save()
+
+    const token = signToken(user._id)
+    res.status(200).json({
+        status: 'success',
+        token
+    })
+
+}
--- models/userModel.js
+++ models/userModel.js
@@ -65,7 +65,7 @@ userSchema.methods.changedPasswordAfter = function( JWTTimestamp ) {
     return false
 }
 
-userSchema.methods.createPasswordResetToken = function() {
+userSchema.methods.createPasswordResetToken = async function() {
     const resetToken = crypto.randomBytes(32).toString('hex')
 
     this.passwordResetToken = await crypto.createHash('sha256').update(resetToken).digest('hex');
@@ -73,7 +73,6 @@ userSchema.methods.createPasswordResetToken = function() {
     this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
     return resetToken
-
 }
 
 const User = mongoose.model('User', userSchema)
