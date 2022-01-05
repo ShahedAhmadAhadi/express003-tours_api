75cefbdadf469583ca0991ccb553a6a2ad87e97e
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-06 02:41:23 +0430

    reset functionality  -> nodemailer package with auths provided and resetTOken

--- controllers/authController.js
+++ controllers/authController.js
@@ -78,3 +78,16 @@ exports.restrictTo = (...roles) => {
         next()
     }
 }
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+    const user = await User.findOne({email: req.body.email})
+    if(!user) {
+        return next(new AppError('There is no user with this email', 404)) 
+    }
+
+    const resetToken = user.createResetPasswordToken()
+    await user.save({validateBeforeSave: false})
+
+})
+
+exports.resetPassword = (req, res, next)
--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto')
 const mongoose = require("mongoose");
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
@@ -37,7 +38,9 @@ const userSchema = new mongoose.Schema({
         },
         select: false
     },
-    passwordChangedAt: Date
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date
 })
 
 userSchema.pre('save', async function(next) {
@@ -60,6 +63,17 @@ userSchema.methods.changedPasswordAfter = function( JWTTimestamp ) {
     }
 
     return false
+}
+
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex')
+
+    this.passwordResetToken = await crypto.createHash('sha256').update(resetToken).digest('hex');
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+    return resetToken
+
 }
 
 const User = mongoose.model('User', userSchema)
