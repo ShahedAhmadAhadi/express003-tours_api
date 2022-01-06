ff6e9b68d888e2e207ebb932bb83012a401da026
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-06 22:44:07 +0430

    api security against most attack and sql injection of mongo

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const rateLimit = require("express-rate-limit")
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require("xss-clean")
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -7,12 +11,25 @@ const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 
 const app = express()
+app.use(helmet())
 console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan("dev"))
 }
 
-app.use(express.json())
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests from this IP. Please try again in an Hour!"
+})
+
+app.use('/api', limiter)
+
+app.use(express.json({limit: '10kb'}))
+
+app.use(mongoSanitize())
+
+app.use(xss())
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
--- models/userModel.js
+++ models/userModel.js
@@ -50,7 +50,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', async function(next) {
     if (!this.isModified('password')) return next();
-
+    
     this.password = await bcrypt.hash(this.password, 12)
     this.passwordConfirm = undefined
     next()
@@ -58,10 +58,15 @@ userSchema.pre('save', async function(next) {
 
 userSchema.pre('save', function (next) {
     if(!this.isModified('password') || this.isNew) return next()
-
+    
     this.passwordChangedAt = Date.now() - 1000
     next()
+    
+})
 
+userSchema.pre(/^find/, function(next) {
+    this.find({active: {$ne: false}})
+    next()
 })
 
 userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
