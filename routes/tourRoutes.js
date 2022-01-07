b91bfa95a60a2406d2b868afa931a27bf0617130
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-07 23:58:52 +0430

    merge params of express for tours review and single review in a tour

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const tourController = require('../controllers/tourController')
+const reviewRouter = require('../routes/reviewRoutes')
 const router = express.Router()
-
 const authController = require('../controllers/authController')
+
+// router.route('/:tourId/reviews').post(authController.protect, authController.restrictTo('user'), reviewController.createReview)
+
+router.use('/:tourId/reviews', reviewRouter)
 
 // router.param('id', tourController.checkID)
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
@@ -12,5 +16,7 @@ router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan)
 
 router.route('/').get(tourController.getAllTours).post(tourController.createTour)
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour)
+
+
 
 module.exports = router
