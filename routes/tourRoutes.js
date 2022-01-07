a6c5deaac96d9d678896586300fe5c3776292880
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-08 01:01:57 +0430

    authentication and authorization completed and new data adding scripts used for other data

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,22 +1,48 @@
-const express = require("express")
-const tourController = require('../controllers/tourController')
-const reviewRouter = require('../routes/reviewRoutes')
-const router = express.Router()
-const authController = require('../controllers/authController')
+const express = require('express');
+const tourController = require('../controllers/tourController');
+const reviewRouter = require('../routes/reviewRoutes');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
 
 // router.route('/:tourId/reviews').post(authController.protect, authController.restrictTo('user'), reviewController.createReview)
 
-router.use('/:tourId/reviews', reviewRouter)
+router.use('/:tourId/reviews', reviewRouter);
+router
+    .route('/top-5-cheap')
+    .get(tourController.aliasTopTours, tourController.getAllTours);
 
-// router.param('id', tourController.checkID)
-router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
+router
+    .route('/tour-stats')
+    .get(authController.protect, tourController.getTourStats);
+router
+    .route('/monthly-plan/:year')
+    .get(
+        authController.protect,
+        authController.restrictTo('admin', 'lead-guide', 'guide'),
+        tourController.getMonthlyPlan
+    );
 
-router.route('/tour-stats').get(authController.protect, tourController.getTourStats)
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan)
+router
+    .route('/')
+    .get(tourController.getAllTours)
+    .post(
+        authController.protect,
+        authController.restrictTo('admin', 'lead-guide'),
+        tourController.createTour
+    );
+router
+    .route('/:id')
+    .get(tourController.getTour)
+    .patch(
+        authController.protect,
+        authController.restrictTo('admin', 'lead-guide'),
+        tourController.updateTour
+    )
+    .delete(
+        authController.protect,
+        authController.restrictTo('admin', 'lead-guide'),
+        tourController.deleteTour
+    );
 
-router.route('/').get(tourController.getAllTours).post(tourController.createTour)
-router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour)
-
-
-
-module.exports = router
+module.exports = router;
