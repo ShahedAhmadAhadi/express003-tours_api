665cbae6a1df714c342e5208ca14e08effaccd0b
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-14 23:13:18 +0430

    validation pracs

---
+++ mongo-validation-p/validation-1.js
@@ -0,0 +1,42 @@
+// this is how you set validations for mongoDB at first that there is not validation added and it also create the collection
+db.createCollection('posts', {
+    validator: {
+      $jsonSchema: {
+        bsonType: 'object',
+        required: ['title', 'text', 'creator', 'comments'],
+        properties: {
+          title: {
+            bsonType: 'string',
+            description: 'must be a string and is required'
+          },
+          text: {
+            bsonType: 'string',
+            description: 'must be a string and is required'
+          },
+          creator: {
+            bsonType: 'objectId',
+            description: 'must be an objectid and is required'
+          },
+          comments: {
+            bsonType: 'array',
+            description: 'must be an array and is required',
+            items: {
+              bsonType: 'object',
+              required: ['text', 'author'],
+              properties: {
+                text: {
+                  bsonType: 'string',
+                  description: 'must be a string and is required'
+                },
+                author: {
+                  bsonType: 'objectId',
+                  description: 'must be an objectid and is required'
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  });
+  
