9cc9b4a5cedafe5e86de47bfe6c95f6e12d38246
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-15 13:43:18 +0430

    email refactored

--- utils/email.js
+++ utils/email.js
@@ -1,5 +1,35 @@
 const nodemailer = require('nodemailer');
 
+// module.exports = class Email {
+//     constructor(user, url) {
+//         this.to = user.email;
+//         this.firstName = user.name.split(' ')
+//         this.url = url;
+//         this.from = `Jonas Schmedtmann ${process.env.EMAIL_FROM}`
+//     }
+
+//     createTransport() {
+//         if(process.env.NODE_ENV = 'production') {
+//             return 1
+//         }
+//         return nodemailer.createTransport({
+//             host: process.env.EMAIL_HOST,
+//             port: process.env.EMAIL_PORT,
+//             auth: {
+//                 user: process.env.EMAIL_USERNAME,
+//                 pass: process.env.EMAIL_PASSWORD
+//             }
+//         });
+//     }
+
+//     send(template, subject) {
+
+//     }
+
+//     sendWelcome
+
+// }
+
 const sendEmail = async options => {
     // 1) Create a transporter
     const transporter = nodemailer.createTransport({
